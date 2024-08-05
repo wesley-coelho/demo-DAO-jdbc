fb12ed95ecb1ca82d172c0abfaa9992263f6480f
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-08-05 14:19:31 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.enities.Department;
 import model.enities.Seller;
 
@@ -11,6 +13,9 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,15 @@
 package model.dao;
 
+import java.util.List;
+
+import model.enities.Department;
+
 public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
 
 }
