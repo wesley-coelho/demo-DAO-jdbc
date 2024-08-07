9c8a864a7012e97aed89bae9850ef07dff145ac8
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-08-07 13:34:47 -0300

    DepartmentDaoJDBC implementation, DaoFactory updated and created Program2 for departmentDaoJDBC tests

---
+++ src/application/Program2.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.enities.Department;
+import model.enities.Seller;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		System.out.println("=== TEST 1: department findById ===");
+		Department department = departmentDao.findById(3);
+		System.out.println(department);		
+		
+		
+		System.out.println("\n=== TEST 2: department findAll ===");
+		List<Department> list = new ArrayList<>();
+		list  = departmentDao.findAll();
+		for(Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== TEST 3: department insert ===");
+		Department newDepartment = new Department(null, "Food");
+		departmentDao.insert(newDepartment);
+		System.out.println("Insered! New id = " + newDepartment.getId());
+		
+		System.out.println("\n=== TEST 4: department update ===");
+		department = departmentDao.findById(6);
+		department.setName("Staff");
+		departmentDao.update(department);
+		System.out.println("Update completed!");
+		
+		System.out.println("\n=== TEST 5: department delete ===");
+		System.out.println("Enter id for delete test: ");
+		
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
+	
+	}	
+
+}
